f58e12e1ef4753bc9b07dc430b7d46a26015fba5
Shafira Putri Novia Hartanti <shafira.putri91@ui.ac.id>
2023-06-28 13:03:31 +0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: [
+          // 'http://notesapp-v1.dicodingacademy.com/',
+          // 'http://localhost:5000/',
+          '*',
+        ],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
